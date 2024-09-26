0dacc386dd59f43fb661cbd031458a380164ccc3
Raul <raul_cosmin_26@yahoo.com>
2024-09-26 22:12:33 +0300

    Add posts page and edit page.

---
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -0,0 +1,11 @@
+import BackButton from "@/components/BackButton";
+
+const EditPage = () => {
+  return (
+    <div>
+      <BackButton text="Back to posts" link="/posts" />
+    </div>
+  );
+};
+
+export default EditPage;
