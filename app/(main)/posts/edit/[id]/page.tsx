3e3a9d245ad85ac7aa62f04cd66425b8d5555797
Raul <raul_cosmin_26@yahoo.com>
2024-09-27 22:53:48 +0300

    Start adding the edit post form.

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -1,11 +1,82 @@
+"use client";
 import BackButton from "@/components/BackButton";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { Button } from "@/components/ui/button";
+import posts from "@/data/posts";
 
-const EditPage = () => {
+const formSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  body: z.string().min(1, { message: "Body is required" }),
+  author: z.string().min(1, { message: "Author is required" }),
+  date: z.string().min(1, { message: "Date is required" }),
+});
+
+interface PostEditPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const PostEditPage = ({ params }: PostEditPageProps) => {
+  const post = posts.find((post) => {
+    return post.id === params.id;
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      title: post?.title || "",
+      body: post?.body || "",
+      author: post?.author || "",
+      date: post?.date || "",
+    },
+  });
+
+  // https://www.youtube.com/watch?v=hhudoSMM0yU&t=5638s
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {};
   return (
     <div>
       <BackButton text="Back to posts" link="/posts" />
+      <h3 className="mb-4 text-2xl">Edit post</h3>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="dark:text-secondary-70 text-xs uppercase text-zinc-500">
+                  Title
+                </FormLabel>
+                <FormControl>
+                  <Input
+                    className="border-0 bg-slate-100 text-black focus-visible:ring-0 focus-visible:ring-offset-0"
+                    placeholder="Enter title"
+                    {...field}
+                  />
+                </FormControl>
+
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 };
 
-export default EditPage;
+export default PostEditPage;
